f416899d261eefdf01ea0bcf0740e7879b130a58
MaxleyCv <maxim.lish@gmail.com>
2020-11-10 11:35:28 +0200

    added implementation of the career task

--- main.py
+++ main.py
@@ -1,77 +1,14 @@
-import copy
 import doctest
+from graph import *
 
 INPUT_FILE = open("career.in", "r")
 OUTPUT_FILE = open("career.out", "w")
 
 
-class Vertex:
-    """
-    Vertex of the graph, has connections that are relative for a particular graph
-    (e.g. one vertex is impossible to use in two different graphs)
-    """
-
-    def __init__(self, val):
-        self.value = val
-        self.connections = []
-        self.pathway = 0
-
-
-class Graph:
-    """
-    Class for iplementation of the graphs
-    Has a list of Vertexes, each vertex has value and maximum pathway property
-    """
-
-    def __init__(self):
-        self.vertexes = []
-        self.max_profit = 0
-
-    def add_vertex(self, val, connections):
-        new_vertex = Vertex(val)
-        new_vertex.connections = copy.deepcopy(connections)
-        self.vertexes.append(new_vertex)
-
-    def breadth_search(self, indexes, is_first=False):
-        """
-        Breadth search part of algorithm, generates next vertexes to search from
-        The search chooses maximum weight path
-        Result of maximum weight path is stored in the graph
-        :param indexes: indexes of the graph
-        :return: nothing
-        >>> G = Graph()
-        >>> G.add_vertex(4, [])
-        >>> G.add_vertex(1, [])
-        >>> G.add_vertex(2, [0, 1])
-        >>> G.add_vertex(3, [2])
-        >>> G.breadth_search([3], is_first=True)
-        >>> G.max_profit
-        9
-        """
-        if is_first:
-            self.vertexes[indexes[0]].pathway = self.vertexes[indexes[0]].value
-        next_indexes = []
-        for index in indexes:
-            current = copy.deepcopy(self.vertexes[index])
-            if current.pathway > self.max_profit:
-                self.max_profit = current.pathway
-            if len(current.connections):
-                for new_index in self.vertexes[index].connections:
-                    next_vertex = copy.deepcopy(self.vertexes[new_index])
-                    if next_vertex.pathway <= current.pathway + next_vertex.value:
-                        self.vertexes[new_index].pathway = current.pathway + next_vertex.value
-                        next_indexes.append(new_index)
-                    else:
-                        continue
-        if len(next_indexes):
-            self.breadth_search(next_indexes)
-        else:
-            return
-
-
 def get_maximum_profit(hierarchy):
     """
-    :param hierarchy: biased self of pyramydial hierarchy
+    The function to implement a search for maximum value path
+    :param hierarchy: graph of pyramidal type
     :return: maximum path weight
     >>> get_maximum_profit(get_items())
     13
@@ -90,13 +27,29 @@ def get_maximum_profit(hierarchy):
 
 
 def generate_indexes(level_length, index, graph_length):
+    """
+    A function to generate connections between the vertexes in a pyramidal type
+    :param level_length: the length of the level of the graph
+    :param index: the index of a vertex in level
+    :param graph_length: maximum index of a vertex before the level was added
+    :return: maximum path weight
+    >>> generate_indexes(4, 2, 11)
+    [9, 8]
+    >>> generate_indexes(5, 1, 6)
+    [2, 1]
+    >>> generate_indexes(2, 1, 7)
+    [6, 5]
+    """
+    if index >= level_length:
+        raise ValueError
+
     new_id = graph_length + index
     return [(new_id - level_length), new_id - level_length - 1]
 
 
 def get_items():
     """
-    A function to read from a file
+    A function to read graph from a file
     :return: Graph of the type as in the task read from input file
     """
     hierarchy = Graph()
